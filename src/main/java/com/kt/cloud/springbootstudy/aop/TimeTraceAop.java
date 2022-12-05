27d99c3b07dc7d044fdc577155d3b59af26cef2f
Yeeun Chung <yeeunchung@Yeeunui-MacBookPro.local>
2022-12-05 17:58:18 +0900

    aop 입력 중

---
+++ src/main/java/com/kt/cloud/springbootstudy/aop/TimeTraceAop.java
@@ -0,0 +1,29 @@
+package com.kt.cloud.springbootstudy.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class TimeTraceAop {
+
+    @Around("execution(* com.kt.cloud.springbootstudy..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+
+        try {
+            System.out.println("try test");
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+
+    }
+
+}
